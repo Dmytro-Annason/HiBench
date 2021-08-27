625ac39bb07946320b1369da5c2f23700a8b54e0
Grigor Avagyan <Grigor.Avagyan@hpe.com>
2021-08-27 11:25:23 +0400

    change version to 3.1.2

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 case "$1" in
     spark)
-      $mvn_home -P sql,sparkbench,websearch,micro,streaming,spark3.1.1.0,ml,structuredStreaming,graph,scala2.12,!defaultScalaVersion,!allModules clean install
+      $mvn_home -P sql,sparkbench,websearch,micro,streaming,spark3.1.2.0,ml,structuredStreaming,graph,scala2.12,!defaultScalaVersion,!allModules clean install
       shift 1;;
 esac
